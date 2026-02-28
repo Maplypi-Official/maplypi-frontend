282394831cdbccd57a7eed9db88d2335e1ecfc50
Mr-X <118984549+EslaM-X@users.noreply.github.com>
2026-02-28 21:36:27 +0200

    Update Crown.tsx

--- src/components/Crown/Crown.tsx
+++ src/components/Crown/Crown.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Crown.css';
 
 const Crown: React.FC<{ logoUrl: string }> = ({ logoUrl }) => (
-  <div className="dashboard-crown">
+  <div className="dashboard-crown-container">
     <div className="logo-outer-circle">
       <div className="logo-inner-circle">
         <img src={logoUrl} alt="MaplyPi Logo" className="central-logo" />
