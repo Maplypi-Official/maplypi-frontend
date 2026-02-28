00dc2f913e3f09d0d36117c04d2f3710e03f2908
Mr-X <118984549+EslaM-X@users.noreply.github.com>
2026-02-28 13:42:31 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx' // تم تغيير الامتداد لـ TSX
 
-createRoot(document.getElementById('root')).render(
+// استخدام الـ (!) لإخبار TypeScript أن العنصر موجود بالتأكيد في index.html
+const rootElement = document.getElementById('root')!;
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
