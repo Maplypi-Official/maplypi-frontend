58bfec1fcf518884f64dd6f0bdff9d20a46d29a3
Mr-X <118984549+EslaM-X@users.noreply.github.com>
2026-03-01 15:07:47 +0200

    Update useMarket.ts

--- src/features/market/hooks/useMarket.ts
+++ src/features/market/hooks/useMarket.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Product } from '../types/market.d'; 
+import { Product } from '../types/market.ts'; 
 import axios from 'axios';
 
 export const useMarket = () => {
